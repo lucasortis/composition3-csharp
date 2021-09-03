ab9324b551fc8234c59342f5444eb1f9e5dbce98
Lucas Ortis <lucasortis01@gmail.com>
2021-09-03 20:57:26 -0300

    Início

---
+++ ComposicaoExercicioProposto/Entities/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ComposicaoExercicioProposto.Entities
+{
+    class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDate { get; set; }
+
+
+    }
+}
---
+++ ComposicaoExercicioProposto/Entities/Order.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ComposicaoExercicioProposto.Entities.Enum;
+
+
+namespace ComposicaoExercicioProposto.Entities
+{
+    class Order
+    {
+        public DateTime Date { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> OrderItens = new List<OrderItem>();
+        public Client Client { get; set; }
+
+        public Order()
+        {
+
+        }
+
+        
+
+    }
+}
